486be26ba6bbc89d4fd298bfe2bbc447117de3c0
ikidata <ikidata>
2024-02-24 10:27:39 +0000

    Added catalog_automation to run.exe notebook
    Updated example code

--- run.exe.py
+++ run.exe.py
@@ -3,6 +3,7 @@
 # MAGIC ## Required input parameters
 # MAGIC * app_id  ||  Service Principal application ID
 # MAGIC * display_name  ||  Databricks will use this display name in the workspace
+# MAGIC * catalog_name  ||  Name of the Databricks workspace catalog which will be used as the main catalog for Ikidata's automation solution
 # MAGIC * server_hostname  ||  Databricks workspace hostname 'https://adb-1234556.1.azuredatabricks.net
 # MAGIC * token   ||   Can be PAT or Entra ID token as long as token owner has admin rights
 
@@ -11,13 +12,14 @@
 # DBTITLE 1,Populate parameters
 app_id = ''
 display_name = ''
+catalog_name = ''
 server_hostname = ''
 token = ''  # Using Azure Key Vault HIGHLY RECOMMENDED: dbutils.secrets.get(scope = '', key = '') 
 
 # COMMAND ----------
 
 # DBTITLE 1,Importing modules
-from modules import service_principal_management, workspace_management, table_management
+from modules import service_principal_management, workspace_management, table_management, catalog_management
 
 # COMMAND ----------
 
@@ -25,14 +27,12 @@ from modules import service_principal_management, workspace_management, table_ma
 service_principal_management(token, server_hostname, app_id, display_name, 'create')
 workspace_management(token, server_hostname, app_id, 'create')
 table_management(token, server_hostname, app_id, 'create')
+catalog_management(token, server_hostname, app_id, catalog_name, 'create')
 
 # COMMAND ----------
 
 # DBTITLE 1,Removing access rights from Service Principal
+catalog_management(token, server_hostname, app_id, catalog_name, 'delete')
 table_management(token, server_hostname, app_id, 'delete')
 workspace_management(token, server_hostname, app_id, 'delete')
 service_principal_management(token, server_hostname, app_id, display_name, 'delete')
-
-# COMMAND ----------
-
-
